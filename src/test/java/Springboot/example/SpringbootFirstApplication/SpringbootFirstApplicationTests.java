8247681072b21f969d41e5837212388f2bb57dce
S Vijaya Lakshmi <vijayalakshmi.svl.0017@gmail.com>
2025-05-22 12:36:57 +0530

    added hello API

---
+++ src/test/java/Springboot/example/SpringbootFirstApplication/SpringbootFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package Springboot.example.SpringbootFirstApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
